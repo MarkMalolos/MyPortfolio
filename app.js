d1889514ffa4e2b3d28645ab8c0e5b88f5078c35
MarkMalolos <mjpmalolos03@gmail.com>
2023-04-26 01:30:03 +0400

    responsive navbar, font for responsive website

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar_menu');
+const mobileMenu = document.getElementById('mobileMenu');
+const navbarMenu = document.getElementById('navbarMenu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
+document.onclick = function(e) {
+    if(e.target.id !== 'mobileMenu' && e.target.id !== 'navbarMenu') {
+        mobileMenu.classList.remove('active');
+        navbarMenu.classList.remove('active');
+    }
+}
+
+mobileMenu.onclick = function() {
+    mobileMenu.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+}
 
 
 
